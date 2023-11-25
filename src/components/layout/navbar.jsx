73e6c5f43648247c111e225e417d03694dcb7152
rifqigenta <rifqigentabuana@students.amikom.ac.id>
2023-11-26 04:47:18 +0700

    landing page beranda, navbar, & footer done

--- src/components/layout/navbar.jsx
+++ src/components/layout/navbar.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import { Link } from "react-router-dom";
 // import Navigation from "react-sticky-nav";
 // import { Navigation } from "react-sticky-nav";
-import LogoNavbar from "../../assets/logo.svg";
+// import LogoNavbar from "../../assets/logo.svg";
 export const Navbar = () => {
   return (
     <>
-      <div className='bg-[#002C02] fixed top-0 left-0 right-0'>
+      <div className='bg-[#002C02] fixed top-0 left-0 right-0 z-50'>
         <div className='px-0 mx-12 text-[22px]'>
           <ul className='flex justify-start items-center m-0 gap-x-12 font-bold text-[#ffffff]'>
             <li>
               <Link to='/'>
-                <img src={LogoNavbar} className='w-[100px] h-[60px]' alt='' />
+                <img src='./beranda/ForEatsz.png' className='h-[70px]' alt='' />
               </Link>
             </li>
             <li>
--- src/pages/landing-page/home.jsx
+++ src/pages/landing-page/home.jsx
@@ -1,24 +1,6 @@
 import React from "react";
 import Navbar from "../../components/layout/navbar";
-import Hero from "../../assets/hero-beranda.svg";
-import Feature1 from "../../assets/feature-home1.svg";
-import Feature2 from "../../assets/feature-home2.svg";
-import Feature3 from "../../assets/feature-home3.svg";
-import Feature4 from "../../assets/feature-home4.svg";
-import Cewe from "../../assets/ceweDiHome.svg";
-import Sayur from "../../assets/sayur.svg";
-import Buah from "../../assets/buah.svg";
-import Bumbu from "../../assets/bumbu.svg";
-import Iwak from "../../assets/iwak.svg";
-import Sembako from "../../assets/sembako.svg";
-import Susu from "../../assets/produksusu.svg";
-import Tahu from "../../assets/tahu.svg";
-import Lainnya from "../../assets/lainnya.svg";
 import Footer from "../../components/footer";
-import Testi1 from "../../assets/testi1.svg";
-import Testi2 from "../../assets/testi2.svg";
-import Testi3 from "../../assets/testi3.svg";
-import Iklan from "../../assets/iklan.svg";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight, faQuoteRight, faStar } from "@fortawesome/free-solid-svg-icons";
@@ -28,8 +10,26 @@ export const Home = () => {
     <>
       <div className='bg-[#002c02]'>
         <Navbar />
-        <div className='carousel pt-[3.5em]'>
-          <img src={Hero} className=' w-screen' alt='' />
+        <div className='carousel mt-[4em] flex items-center'>
+          <div className='mx-14'>
+            <div className='font-semibold text-white text-[48px]'>
+              <p>Sehat & Segar</p>
+              <p>Bikin Kamu Semangat</p>
+            </div>
+            <div className='flex items-center mt-4'>
+              <div class='h-[56px] w-[2px] bg-[#00b207] me-6'></div>
+              <div className='flex-col'>
+                <div className='font-medium text-white  py-0 text-[24px]'>
+                  <p className='opacity-[60%] '>DISKON HINGGA</p>
+                  <p>48%</p>
+                </div>
+              </div>
+            </div>
+            <button class='bg-[#00b207] hover:bg-[#009505] text-white text-[16px] font-semibold py-4 px-10 mt-10 rounded-full'>
+              Belanja Sekarang &ensp;
+              <FontAwesomeIcon icon={faArrowRight} />
+            </button>
+          </div>
         </div>
         <div className='mx-12 absolute top-[103%] left-0 right-0'>
           {/* <div className='mx-12 relative -mt-6'> */}
@@ -38,7 +38,7 @@ export const Home = () => {
               <div className='w-[312px] rounded-[10px] text-center py-12 px-0'>
                 <div className='flex flex-col'>
                   <div className='mx-auto'>
-                    <img className='mx-auto mb-4' src={Feature1} alt='' />
+                    <img className='mx-auto mb-4' src='./beranda/feature-home1.svg' alt='' />
                     <p className='text-[18px] font-semibold mb-2'>Bebas Biaya Kirim</p>
                     <p className='text-[14px] text-[#808080]'>Gratis Ongkos Kirim Untuk Semua Pesanan Anda</p>
                   </div>
@@ -47,7 +47,7 @@ export const Home = () => {
               <div className='w-[312px] rounded-[10px] text-center py-12 px-4'>
                 <div className='flex flex-col'>
                   <div className='mx-auto'>
-                    <img className='mx-auto mb-4' src={Feature2} alt='' />
+                    <img className='mx-auto mb-4' src='./beranda/feature-home2.svg' alt='' />
                     <p className='text-[18px] font-semibold mb-2'>Dukungan Pelanggan 24/7</p>
                     <p className='text-[14px] text-[#808080]'>Akses Instant Kedukungan</p>
                   </div>
@@ -56,7 +56,7 @@ export const Home = () => {
               <div className='w-[312px] rounded-[10px] text-center py-12 px-4'>
                 <div className='flex flex-col'>
                   <div className='mx-auto'>
-                    <img className='mx-auto mb-4' src={Feature3} alt='' />
+                    <img className='mx-auto mb-4' src='./beranda/feature-home3.svg' alt='' />
                     <p className='text-[18px] font-semibold mb-2'>100% Pembayaran Aman</p>
                     <p className='text-[14px] text-[#808080]'>Kami Memastikan Uang Anda Aman</p>
                   </div>
@@ -65,7 +65,7 @@ export const Home = () => {
               <div className='w-[312px] rounded-[10px] text-center py-12 px-4'>
                 <div className='flex flex-col'>
                   <div className='mx-auto'>
-                    <img className='mx-auto mb-4' src={Feature4} alt='' />
+                    <img className='mx-auto mb-4' src='./beranda/feature-home4.svg' alt='' />
                     <p className='text-[18px] font-semibold mb-2'>Garansi Uang Kembali</p>
                     <p className='text-[14px] text-[#808080]'>Jaminan uang kembali 30 hari</p>
                   </div>
@@ -74,10 +74,10 @@ export const Home = () => {
             </div>
           </div>
         </div>
-        <div className='h-[670px] mt-[150px] bg-[#E7F4E8]'>
+        <div className='welcome h-[670px] mt-[110px] bg-[#E7F4E8]'>
           <div className='mx-12'>
             <div className='flex mx-24 pt-[140px]'>
-              <img src={Cewe} alt='' />
+              <img src='./beranda/ceweDiHome.svg' alt='' />
               <div className='mx-24 py-24'>
                 <p className=' text-[15px] text-[#00B207]'>Hallo</p>
                 <p className='text-[#002603] font-semibold text-[38px]'>Selamat datang di</p>
@@ -99,25 +99,25 @@ export const Home = () => {
             <div className='flex justify-between gap-8'>
               <div className='w-[250px] bg-white rounded-[10px] flex flex-col pt-8 pb-4'>
                 <div className='mx-auto text-center font-bold text-[24px]'>
-                  <img src={Sayur} alt='' />
+                  <img src='./beranda/sayur.svg' alt='' />
                   <p className='mt-4'>Sayur</p>
                 </div>
               </div>
               <div className='w-[250px] bg-white rounded-[10px] flex flex-col pt-8 pb-4'>
                 <div className='mx-auto text-center font-bold text-[24px]'>
-                  <img src={Buah} alt='' />
+                  <img src='./beranda/buah.svg' alt='' />
                   <p className='mt-4'>Buah</p>
                 </div>
               </div>
               <div className='w-[250px] bg-white rounded-[10px] flex flex-col pt-8 pb-4'>
                 <div className='mx-auto text-center font-bold text-[24px]'>
-                  <img src={Bumbu} alt='' />
+                  <img src='./beranda/bumbu.svg' alt='' />
                   <p className='mt-4'>Bumbu</p>
                 </div>
               </div>
               <div className='w-[250px] bg-white rounded-[10px] flex flex-col pt-8 pb-4'>
                 <div className='mx-auto text-center font-bold text-[24px]'>
-                  <img src={Iwak} alt='' />
+                  <img src='./beranda/iwak.svg' alt='' />
                   <p className='mt-4'>Daging & Ikan</p>
                 </div>
               </div>
@@ -125,32 +125,32 @@ export const Home = () => {
             <div className='flex justify-between gap-8 mt-8'>
               <div className='w-[250px] bg-white rounded-[10px] flex flex-col pt-8 pb-4'>
                 <div className='mx-auto text-center font-bold text-[24px]'>
-                  <img src={Sembako} alt='' />
+                  <img src='./beranda/sembako.svg' alt='' />
                   <p className='mt-4'>Sembako</p>
                 </div>
               </div>
               <div className='w-[250px] bg-white rounded-[10px] flex flex-col pt-8 pb-4'>
                 <div className='mx-auto text-center font-bold text-[24px]'>
-                  <img src={Susu} alt='' />
+                  <img src='./beranda/produksusu.svg' alt='' />
                   <p className='mt-4'>Produk Susu</p>
                 </div>
               </div>
               <div className='w-[250px] bg-white rounded-[10px] flex flex-col pt-8 pb-4'>
                 <div className='mx-auto text-center font-bold text-[24px]'>
-                  <img src={Tahu} alt='' />
+                  <img src='./beranda/tahu.svg' alt='' />
                   <p className='mt-4'>Telur, Tahu, Tempe</p>
                 </div>
               </div>
               <div className='w-[250px] bg-white rounded-[10px] flex flex-col pt-8 pb-4'>
                 <div className='mx-auto text-center font-bold text-[24px]'>
-                  <img src={Lainnya} alt='' />
+                  <img src='./beranda/tahu.svg' alt='' />
                   <p className='mt-4'>Lain - lain</p>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div className='h-[670px] bg-[#E7F4E8]'>
+        <div className='testi h-[670px] bg-[#E7F4E8]'>
           <div className='mx-12'>
             <fieldset className='border-t-[3px] border-[#000000] mb-5'>
               <legend className='mx-auto px-5 font-bold text-[34px] text-black py-24'>Testimoni Pelanggan</legend>
@@ -163,7 +163,7 @@ export const Home = () => {
                   kantor. Bisa jga ke alamat rumah atau lokasi lainnya. Ga mesti macet2an atau ribet deh! Cari sayur dan buah segar? Di @For Eat_official aja
                 </p>
                 <div className='flex items-center mt-6'>
-                  <img src={Testi1} alt='' />
+                  <img src='./beranda/testi1.svg' alt='' />
                   <div className='flex-col ms-4'>
                     <p className='text-[16px] font-medium text-[#1a1a1a]'>Robert Fox</p>
                     <p className='font-medium text-[14px] text-[#999999]'>Pelanggan</p>
@@ -184,7 +184,7 @@ export const Home = () => {
                   sayur, buah, bumbu dapur, lauk pauk, dsb yg pasti semua barang yg di sediakan fresh dan segar
                 </p>
                 <div className='flex items-center mt-6'>
-                  <img src={Testi2} alt='' />
+                  <img src='./beranda/testi2.svg' alt='' />
                   <div className='flex-col ms-4'>
                     <p className='text-[16px] font-medium text-[#1a1a1a]'>Dianne Russell</p>
                     <p className='font-medium text-[14px] text-[#999999]'>Pelanggan</p>
@@ -205,7 +205,7 @@ export const Home = () => {
                   jadi pengen belanja trus ⠀ Buat kalian yg cari sayuran secara online tgl lgs aja ke websitenya @ForEat www.ForEat.com
                 </p>
                 <div className='flex items-center mt-6'>
-                  <img src={Testi3} alt='' />
+                  <img src='./beranda/testi3.svg' alt='' />
                   <div className='flex-col ms-4'>
                     <p className='text-[16px] font-medium text-[#1a1a1a]'>Eleanor Pena</p>
                     <p className='font-medium text-[14px] text-[#999999]'>Pelanggan</p>
@@ -225,7 +225,16 @@ export const Home = () => {
         <div className='h-[421px]'>
           <div className='mx-12'>
             <div className='mx-12 py-20'>
-              <img src={Iklan} alt='' />
+              <div className='iklan h-[280px]'>
+                <div className='text-[50px] font-bold text-[#0E4A22] mx-24 pt-6'>
+                  <p>Makin Hemat</p>
+                  <p>Dengan Promo 12.12</p>
+                  <button class='bg-[#00b207] hover:bg-[#009505] text-white text-[16px] font-semibold py-4 px-10 rounded-full'>
+                    Belanja Sekarang &ensp;
+                    <FontAwesomeIcon icon={faArrowRight} />
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
