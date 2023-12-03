2d8c6dc09ca48eb8adfd90826c830c076457fbbc
rifqigenta <rifqigentabuana@students.amikom.ac.id>
2023-12-03 16:48:37 +0700

    sidebar fixed

--- src/components/layout/AdminSidebar.jsx
+++ src/components/layout/AdminSidebar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useContext, createContext } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faHouse,
@@ -18,17 +18,19 @@ import {
   faChevronRight,
   faArrowRightFromBracket,
 } from "@fortawesome/free-solid-svg-icons";
+const SidebarContext = createContext();
 
 const MenuCollapse = ({ menus, title, setMenuActive }) => {
+  const { expanded } = useContext(SidebarContext);
   return (
     <>
       <div className='stock'>
         <ul className='menu px-0'>
           <li className=''>
-            <details open>
+            <details closed icon={`${expanded ? "" : "hidden"}`}>
               <summary className='bg-transparent text-[22px] hover:bg-transparent px-2'>
                 <FontAwesomeIcon icon={faClipboardList} className='me-2' />
-                {title}
+                <span className={`${expanded ? "" : "hidden"}`}>{title}</span>
               </summary>
               <ul>
                 {menus.map((value, index) => {
@@ -59,6 +61,7 @@ const MenuCollapse = ({ menus, title, setMenuActive }) => {
 };
 
 const Dashboard = ({ title }) => {
+  const { expanded } = useContext(SidebarContext);
   return (
     <>
       <div className='Dashboard'>
@@ -66,7 +69,7 @@ const Dashboard = ({ title }) => {
           <li>
             <summary className='text-[22px] hover:bg-transparent px-1'>
               <FontAwesomeIcon icon={faHouse} />
-              {title}
+              <span className={`${expanded ? "" : "hidden"}`}>{title}</span>
             </summary>
           </li>
         </ul>
@@ -75,6 +78,7 @@ const Dashboard = ({ title }) => {
   );
 };
 const UserAccount = ({ title }) => {
+  const { expanded } = useContext(SidebarContext);
   return (
     <>
       <div className='Dashboard'>
@@ -82,7 +86,7 @@ const UserAccount = ({ title }) => {
           <li>
             <summary className='text-[22px] hover:bg-transparent px-1'>
               <FontAwesomeIcon icon={faUserPen} />
-              {title}
+              <span className={`${expanded ? "" : "hidden"}`}>{title}</span>
             </summary>
           </li>
         </ul>
@@ -91,6 +95,7 @@ const UserAccount = ({ title }) => {
   );
 };
 const Report = ({ title }) => {
+  const { expanded } = useContext(SidebarContext);
   return (
     <>
       <div className='Dashboard'>
@@ -98,7 +103,7 @@ const Report = ({ title }) => {
           <li>
             <summary className='text-[22px] hover:bg-transparent px-1'>
               <FontAwesomeIcon icon={faFlag} />
-              {title}
+              <span className={`${expanded ? "" : "hidden"}`}>{title}</span>
             </summary>
           </li>
         </ul>
@@ -107,6 +112,7 @@ const Report = ({ title }) => {
   );
 };
 const Logout = ({ title }) => {
+  const { expanded } = useContext(SidebarContext);
   return (
     <>
       <div className='Dashboard'>
@@ -114,7 +120,7 @@ const Logout = ({ title }) => {
           <li>
             <summary className='text-[22px] hover:bg-transparent px-1'>
               <FontAwesomeIcon icon={faArrowRightFromBracket} />
-              {title}
+              <span className={` ${expanded ? "" : "hidden ease-in"}`}>{title}</span>
             </summary>
           </li>
         </ul>
@@ -172,20 +178,52 @@ const AdminSidebar = () => {
     });
     setMenuState(updatedMenus);
   };
+  // const toggleSidebarMin = () => {
+  //   return (
+  //     <>
+  //       <div className='w-[80px] h-screen overflow-y-scroll no-scrollbar bg-[#191919] left-0 py-10 px-4'>
+  //         <div className='grid gap-y-4 text-white'>
+  //           <img src='././beranda/ceweDiHome.svg' className='h-[100px] rounded-full m-auto' alt='' />
+  //           <h6 className='text-[22px] m-auto mb-4'>Admin </h6>
+  //           <Dashboard title='' />
+  //           <MenuCollapse menus={menuState} title='Stock' setMenuActive={setMenuActive} />
+  //           <UserAccount title='' />
+  //           <Report title='' />
+  //           <button className='m-auto w-[50px] h-[50px] btn bg-[#E7F4E8] rounded-full'>
+  //             <FontAwesomeIcon icon={faChevronRight} className='text-[#008E06] text-[22px]' />
+  //           </button>
+  //           <Logout title='Logout' />
+  //         </div>
+  //       </div>
+  //     </>
+  //   );
+  // };
+  const [expanded, setExpanded] = useState(true);
   return (
     <>
-      <div className='w-[280px] h-screen overflow-y-scroll no-scrollbar bg-[#191919] left-0 py-10 px-10'>
-        <div className='grid gap-y-4 text-white'>
-          <img src='././beranda/ceweDiHome.svg' className='h-[100px] rounded-full m-auto' alt='' />
-          <h6 className='text-[22px] m-auto mb-4'>Admin </h6>
-          <Dashboard title='Dashboard' />
-          <MenuCollapse menus={menuState} title='Stock' setMenuActive={setMenuActive} />
-          <UserAccount title='User Account' />
-          <Report title='Report' />
-          <button className='btn'>kdjfak</button>
-          <Logout title='Logout' />
+      <SidebarContext.Provider value={{ expanded }}>
+        <div
+          className={`w-[280px] h-screen overflow-y-scroll no-scrollbar bg-[#191919] left-0 py-10 px-10  transition-opacity duration-500 
+        ${expanded ? "" : "w-[75px] h-screen px-4"}`}
+        >
+          <div className='grid gap-y-4 text-white'>
+            <img src='././beranda/ceweDiHome.svg' className={`h-[100px] rounded-full m-auto ${expanded ? "" : "h-[50px] rounded-full mx-0"}`} alt='' />
+            <h6 className={`text-[22px] m-auto mb-4 ${expanded ? "" : "hidden"}`}>Admin </h6>
+            <Dashboard title='Dashboard' />
+            <MenuCollapse menus={menuState} title='Stock' setMenuActive={setMenuActive} />
+            <UserAccount title='User Account' />
+            <Report title='Report' />
+            <button className={`m-auto w-[50px] h-[50px] btn bg-[#E7F4E8] rounded-full ${expanded ? "" : "w-[15px] mx-0"}`} onClick={() => setExpanded((curr) => !curr)}>
+              {expanded ? (
+                <FontAwesomeIcon icon={faChevronRight} className='text-[#008E06] text-[22px] ease-in' />
+              ) : (
+                <FontAwesomeIcon icon={faChevronLeft} className='text-[#008E06] text-[15px] ease-out' />
+              )}
+            </button>
+            <Logout title='Logout' />
+          </div>
         </div>
-      </div>
+      </SidebarContext.Provider>
     </>
   );
 };
